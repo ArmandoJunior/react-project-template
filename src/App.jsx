819a8f65e767a2297aaacaae106bcf718d42d83d
Armando N Junior <armandojrn@hotmail.com>
2021-05-31 19:23:37 -0300

    add scss to repositoryList

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
     // throw new Error('Eita Giovana, o forninho caiu!')
     return (
-        <>
-            <RepositoryList />
-            <Counter />
-        </>
+        <RepositoryList />
     )
 }

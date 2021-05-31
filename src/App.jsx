8d10df9e16dd6e37cbf70e156a7adc503dee25f0
Armando N Junior <armandojrn@hotmail.com>
2021-05-31 15:01:40 -0300

    conceito de componete no reactr

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
     // throw new Error('Eita Giovana, o forninho caiu!')
-    return <h1>Hello World App - React</h1>
+    return <RepositoryList />
 }
